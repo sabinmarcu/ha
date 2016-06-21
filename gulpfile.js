35c4d8616f05923b61286efd5e35aeeb878fca4d
Andrei Hodorog <hodorog.andrei@gmail.com>
2016-06-21 21:27:09 +0300

    added slider and some responsive fixes

--- gulpfile.js
+++ gulpfile.js
@@ -4,85 +4,114 @@ var gulp = require('gulp');
 var browserSync = require('browser-sync');
 
 // Load plugins
-var $ = require('gulp-load-plugins')();
+var $ = require('gulp-load-plugins')({
+    rename: {
+            'gulp-ruby-sass': 'sass'    
+        }
+    });
 
 
-gulp.task('styles', function() {
-  var browsers = [
-    '> 1%',
-    'last 2 versions',
-    'Firefox ESR',
-    'Opera 12.1'
-  ];
-  return gulp.src('src/**/*.less')
-    .pipe($.less({
-      paths: ['bower_components']
-    })
-    .on('error', $.util.log))
-    .pipe($.postcss([
-        require('autoprefixer-core')({
-          browsers: browsers
+gulp.task('scss', function(){
+    var browsers = [
+        '> 1%',
+        'last 2 versions',
+        'Firefox ESR',
+        'Opera 12.1'
+    ];
+
+    return gulp.src(['src/**/*.scss'])
+        .pipe($.sass({
+            style: 'expanded'
         })
-      ]))
-    .pipe(gulp.dest('build'))
-    .pipe(browserSync.reload({stream: true}));
+        .on('error', $.util.log))
+        .pipe($.postcss([
+            require('autoprefixer-core')({
+                browsers: browsers
+            })
+        ]))
+        .pipe(gulp.dest('build'))
+        .pipe(browserSync.reload({stream: true}));
 });
+
+gulp.task('less', function() {
+    var browsers = [
+        '> 1%',
+        'last 2 versions',
+        'Firefox ESR',
+        'Opera 12.1'
+    ];
+
+    return gulp.src('src/**/*.less')
+        .pipe($.less({
+            paths: ['bower_components']
+        })
+        .on('error', $.util.log))
+        .pipe($.postcss([
+                require('autoprefixer-core')({
+                    browsers: browsers
+                })
+            ]))
+        .pipe(gulp.dest('build'))
+        .pipe(browserSync.reload({stream: true}));
+    })
+
+gulp.task('styles', ['scss', 'less']);
 
 
 gulp.task('views', function(){
-  return gulp.src([
-      '!src/views/layout.jade',
-      'src/views/*.jade'
-    ])
-    .pipe($.jade({
-      pretty: true
-    }))
-    .on('error', $.util.log)
-    .pipe(gulp.dest('build'))
-    .pipe(browserSync.reload({stream: true}));
+    return gulp.src([
+            '!src/views/layout.jade',
+            'src/views/*.jade'
+        ])
+        .pipe($.jade({
+            pretty: true
+        }))
+        .on('error', $.util.log)
+        .pipe(gulp.dest('build'))
+        .pipe(browserSync.reload({stream: true}));
 });
 
 
 gulp.task('images', function() {
-  return gulp.src('src/images/**/*')
-    .pipe($.imagemin({
-      svgoPlugins: [{
-        convertPathData: false
-      }]
-    }))
-    .pipe(gulp.dest('build/images'));
+    return gulp.src('src/images/**/*')
+        .pipe($.imagemin({
+            svgoPlugins: [{
+                convertPathData: false
+            }]
+        }))
+        .pipe(gulp.dest('build/images'));
 });
 
 
 gulp.task('browser-sync', function() {
-  browserSync({
-    server: {
-      baseDir: './build'
-    }
-  });
+    browserSync({
+        server: {
+            baseDir: './build'
+        }
+    });
 });
 
 
 gulp.task('watch', ['build'], function() {
-  gulp.watch('src/**/*.less', ['styles']);
-  gulp.watch('src/images/**/*', ['images']);
-  gulp.watch('src/**/*.jade', ['views']);
+    gulp.watch('src/**/*.less', ['styles']);
+    gulp.watch('src/images/**/*', ['images']);
+    gulp.watch('src/**/*.jade', ['views']);
 
-  gulp.start('browser-sync');
+    gulp.start('browser-sync');
 });
 
 // JSHint grunfile.js
 gulp.task('selfcheck', function() {
-  return gulp.src('gulpfile.js')
-    .pipe($.jshint())
-    .pipe($.jshint.reporter('default'))
-    .pipe($.jshint.reporter('fail'));
+    return gulp.src('gulpfile.js')
+        .pipe($.jshint())
+        .pipe($.jshint.reporter('default'))
+        .pipe($.jshint.reporter('fail'));
 });
 
 
 gulp.task('clean', function(cb) {
-  var del = require('del');
-  del(['build'], cb);
+    var del = require('del');
+    del(['build'], cb);
 });
 
 
@@ -90,5 +119,5 @@ gulp.task('build', ['styles', 'views', 'images']);
 
 
 gulp.task('default', ['clean'], function() {
-  gulp.start('watch');
+    gulp.start('watch');
 });
