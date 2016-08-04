2ce2bc97cdc0b29faedac9d5f48e3eac6021ad17
Sabin Marcu <sabinmarcu@gmail.com>
2016-08-04 14:45:50 +0300

    fixed map, added routes (& Geocoding)

--- src/js/maps.js
+++ src/js/maps.js
@@ -1,10 +1,44 @@
 var map;
 
 function initMap() {
-  var chicago = new google.maps.LatLng(41.850, -87.650);
+    var chicago = new google.maps.LatLng(41.850, -87.650);
 
-  map = new google.maps.Map(document.getElementById('google-map'), {
-    center: chicago,
-    zoom: 18
-  });
+    var directionsService = new google.maps.DirectionsService();
+    var directionsDisplay = new google.maps.DirectionsRenderer();
+    var geocoder = new google.maps.Geocoder();
+
+    map = new google.maps.Map(document.getElementById('google-map'), {
+        center: chicago,
+        zoom: 18
+    });
+
+    directionsDisplay.setMap(map);
+
+    var stringToLatLngPlaces = function(str, cb) {
+        geocoder.geocode({'address': str}, function(results, status) {
+            if (status === 'OK') {
+                var place = results[0].geometry.location;
+                cb && cb(place);
+            }
+        });
+    }
+    
+    stringToLatLngPlaces("chicago", function(start) {
+        stringToLatLngPlaces("new york", function(end) {
+            var request = {
+                origin: start,
+                destination: end,
+                travelMode: google.maps.TravelMode.DRIVING
+            };
+
+            directionsService.route(request, function(result, status) {
+                console.log(status, result);
+                if (status == google.maps.DirectionsStatus.OK) {
+                    directionsDisplay.setDirections(result);
+                }
+            });
+
+        });
+    });
+
 }
